db5f5b1d85ca6489db4d9324e3d1b286c46c1627
Sunnefoula <60777485+Sunnefoula@users.noreply.github.com>
2022-04-05 13:56:16 +0200

    Revert "my first commit"
    This reverts commit 4412ea6819b2705244c2fc41b2cf68e67679b849.

--- app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-class Config:
-    DB_HOST = os.getenv("DB_HOST", "my.database.com")
-    DB_USERNAME = os.getenv("DB_USERNAME")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
--- app/exception.py
+++
@@ -1,3 +0,0 @@
-class UserNotFound(Exception):
-    def __int__(self, user_id):
-        self.user_id = user_id
